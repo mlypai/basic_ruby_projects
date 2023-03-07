8c200b357d56dde36290c50cf0ae7e6419cc00cf
mlypai <balladbell@gmail.com>
2023-03-07 23:22:32 +0200

    Adding method for drawing board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# creates player for a game #
 class Player
   attr_reader :name, :marker
 
@@ -8,13 +11,30 @@ class Player
   end
 end
 
+# executes all steps necessary for a game #
 class Game
   def initialize
     puts 'Enter first player name: '
     @player1 = Player.new(gets.chomp, 'X')
     puts 'Enter second playersname: '
     @player2 = Player.new(gets.chomp, 'O')
+    @board = Array.new(3) { |i| Array.new(3) { |j| (j + 1) + (3 * i) } }
+  end
+
+  def start
+    drawing_board(@board)
+  end
+
+  def drawing_board(board)
+    13.times { print '-' }
+    print "\n"
+    board.each do |elem|
+      puts "| #{elem[0]} | #{elem[1]} | #{elem[2]} |"
+      13.times { print '-' }
+      print "\n"
+    end
   end
 end
 
-Game.new
+new_game = Game.new
+new_game.start()
